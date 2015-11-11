9cd6ac6610e05399eb7cd8a7fc64e96618e5c4d9
loug38 <loug38@gmail.com>
2015-11-10 17:39:39 -0800

    Working on login screen
    Made login screen the main screen. Use username admin and password admin
    to login succesfully.

--- app/src/main/java/com/example/lou/down/LoginClass.java
+++ app/src/main/java/com/example/lou/down/LoginClass.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 
 /**
@@ -17,18 +18,16 @@ public class LoginClass extends Activity {
         setContentView(R.layout.login_layout);
     }
 
-    EditText username = (EditText) findViewById(R.id.eventUsername);
-    EditText password = (EditText) findViewById(R.id.eventPassword);
-
     public void login_event(View v) {
+        EditText username = (EditText) findViewById(R.id.eventUsername);
+        EditText password = (EditText) findViewById(R.id.eventPassword);
         if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-
+            Intent a = new Intent(this, MainActivity.class );
+            startActivity(a);
             //correcct password
         } else {
             //wrong password
         }
-
-
     }
 }
 
